d844e65d5883cff213121aa749f7d0941b7fcb2d
Manoranjan-Basantia <manoranjan.work25@gmail.com>
2025-09-04 11:05:33 +0530

    updated code

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ const responseMessages = require("./middlewares/response.middleware");
 const handleNotFound = require("./middlewares/notFound.middleware");
 const errorHandler = require("./middlewares/errorHandler.middleware");
 require("./models");
+const cors =require("cors");
 
 const app = express();
 const port = process.env.APP_PORT;
 const baseUrl = process.env.BASE_URL;
 
+// CORS configuration .................
+app.use(cors({
+  origin: '*', // Allow all origins, or specify allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true,}));
+app.use(express.urlencoded({extended: false,}));
 app.use(responseMessages);
 
 
